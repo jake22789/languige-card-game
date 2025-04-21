9e74c443b001abb7f00927ca53d5a3db508aa644
Bradley Mccormick <bradley.mccormick@students.snow.edu>
2025-04-21 11:23:56 -0600

    javascript is setup to be built

---
+++ client/javasccript/board-ui.js
@@ -0,0 +1,11 @@
+import { buildcard } from "./domain.js";
+
+function drawHand(){
+
+}
+function renderSelected(){
+
+}
+function renderGameInfo(){
+    
+}
---
+++ client/javasccript/domain.js
@@ -0,0 +1,6 @@
+export function buildcard(){
+    const card = document.createElement("div");
+    card.classList = "card";
+    const word = document.createElement("div");
+    word.textContent = "word"
+}
