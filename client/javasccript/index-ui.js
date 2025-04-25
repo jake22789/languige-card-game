6763a44c083de8afcf1e9b8b79706edeeae8428d
Bradley Mccormick <bradley.mccormick@students.snow.edu>
2025-04-25 10:32:27 -0600

    quit button working

--- client/javasccript/index-ui.js
+++ client/javasccript/index-ui.js
@@ -3,7 +3,11 @@
 function addevents(){
     const playButtonElement = document.getElementById("play");
     const settingsButtonElement = document.getElementById("settings");
+    const quitButtonElement = document.getElementById("quit");
     
+    quitButtonElement.addEventListener("click",(e)=>{
+        self.close();
+    });
     console.log(playButtonElement);
     playButtonElement.addEventListener("click",(e)=>{
         window.location.href = "board.html";
